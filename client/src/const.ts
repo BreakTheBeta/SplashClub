a4437e90e3faa1fc293935e9836ea113c25b8f62
Will White <willl.b.white@gmail.com>
2025-05-20 11:15:19 +1000

    ww/devices on the network can now connect

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,3 +1,6 @@
-const WS_URL = 'ws://localhost:6969'; // Use localhost or your specific server address
+
+const wsHost = window.location.hostname;
+
+const WS_URL = `ws://${wsHost}:6969`; // Use localhost or your specific server address
 
 export { WS_URL}
